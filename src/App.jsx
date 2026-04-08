47527f60526ce55c77e30f9eb2dd630a14122f6f
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-08 13:26:32 +0300

    add fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList';
 
-const posts = postsFromServer.map(post => {
-  const userPost = usersFromServer.find(user => user.id === post.userId);
-  const commentsPost = commentsFromServer.filter(
-    coment => coment.postId === post.id,
+export const App = () => {
+  const posts = postsFromServer.map(post => {
+    const userPost = usersFromServer.find(user => user.id === post.userId);
+    const commentsPost = commentsFromServer.filter(
+      coment => coment.postId === post.id,
+    );
+
+    return { ...post, user: userPost, comments: commentsPost };
+  });
+
+  return (
+    <section className="App">
+      <h1 className="App__title">Static list of posts</h1>
+      <PostList posts={posts} />
+    </section>
   );
-
-  return { ...post, user: userPost, comments: commentsPost };
-});
-
-export const App = () => (
-  <section className="App">
-    <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={posts} />
-  </section>
-);
+};
